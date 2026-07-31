81a7ff536076c917d045128cbe03bcbecedb0dc4
przemas230 <przemas230@gmail.com>
2026-07-31 16:48:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dieta-app-v1";
+const CACHE_NAME = "dieta-app-v2";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
