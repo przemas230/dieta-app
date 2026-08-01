f93b928c174e04970f786687c9c714c5db8fec64
przemas230 <przemas230@gmail.com>
2026-08-01 09:29:01 +0200

    Update cache name from v5 to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dieta-app-v5";
+const CACHE_NAME = "dieta-app-v7";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
