f6c9cab8703917076ab512113a4929bda06f41e6
przemas230 <przemas230@gmail.com>
2026-08-01 09:11:51 +0200

    Update CACHE_NAME to dieta-app-v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dieta-app-v2";
+const CACHE_NAME = "dieta-app-v5";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
